99b082ea646104034721fad505ca7d04ccdec7bd
MishDM <mish@g.com>
2022-07-19 01:43:36 -0700

    fix eslinterc

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const menuItem = document.querySelector('.menuItem');
 const hambergerClose = document.querySelector('.hambergerClose');
 const hambergerOpen = document.querySelector('.hambergerOpen');
 const menuItems = document.querySelectorAll('nav .menuItem li a');
-hambergerOpen.addEventListener('click', show);
-hambergerClose.addEventListener('click', close);
 // close menu when you click on a menu item 
 menuItems.forEach(item => {
     item.addEventListener('click', function () {
         close();
-    })
-})
+    });
+});
 
 function show() {
     menuItem.style.display = 'flex';
     menuItem.style.top = '0';
-}
+};
+hambergerOpen.addEventListener('click', show);
 function close() {
     menuItem.style.top = '-101%';
-}
+};
+hambergerClose.addEventListener('click', close);
