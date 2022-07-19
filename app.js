aa3164bb450f4fe2ad624f37a610a07554a9ba74
MishDM <mish@g.com>
2022-07-19 14:09:54 -0700

    add fix baesed on figma design

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const menuItem = document.querySelector('.menuItem');
-const hambergerClose = document.querySelector('.hambergerClose');
-const hambergerOpen = document.querySelector('.hambergerOpen');
-const menuItems = document.querySelectorAll('nav .menuItem li a');
-// close menu when you click on a menu item 
-menuItems.forEach(item => {
-    item.addEventListener('click', function () {
-        close();
-    });
-});
-function show() {
-    menuItem.style.display = 'flex';
-    menuItem.style.top = '0';
-}
-hambergerOpen.addEventListener('click', show);
-function close() {
-    menuItem.style.top = '-101%'
-}
-hambergerClose.addEventListener('click', close);
+const mobileNav = document.querySelector('.menuNav');
+const navHeader = document.querySelector('.header');
+const toggleNavbar = () => {
+    navHeader.classList.toggle('active');
+};
+
+mobileNav.addEventListener('click', () => toggleNavbar());
