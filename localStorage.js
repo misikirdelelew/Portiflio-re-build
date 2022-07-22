d1fd8327f4aac280f5f6f36ea9eb95bad904d856
MishDM <mish@g.com>
2022-07-22 15:17:10 -0700

    fix eslint

--- localStorage.js
+++ localStorage.js
@@ -1,5 +1,10 @@
+
 const Iname = document.getElementById('uname');
 const msgbox = document.getElementById('msg-box');
+const form = document.getElementById('form-contact');
+const email = document.getElementById('email');
+const inputName = document.getElementById('name');
+
 form.addEventListener('input', () => {
     localStorage.setItem('Name', `${Iname.value}`);
     localStorage.setItem('Email', `${email.value}`);
